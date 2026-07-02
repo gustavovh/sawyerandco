c3f97dbc4ef2321208491b099c43614ca8834df4
gustavovh <68474381+gustavovh@users.noreply.github.com>
2026-07-02 18:24:01 -0300

    Create PreQualifyForm.jsx

---
+++ frontend/src/components/sections/PreQualifyForm.jsx
@@ -0,0 +1,14 @@
+export default function PreQualifyForm() {
+  return (
+    <section id="prequalify" className="section bg-white">
+      <div className="container-x">
+        <iframe
+          src="https://api.leadconnectorhq.com/widget/form/0vodT8LE8uLNLMKeFbnvx"
+          style={{ width: "100%", height: "600px", border: "none" }}
+          id="ghl-prequalify-form"
+          title="Mortgage Pre-Qualification Form"
+        ></iframe>
+      </div>
+    </section>
+  );
+}
