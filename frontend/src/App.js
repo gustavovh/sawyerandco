a4ae00fd45a08f09678c5196e8746dcbf8da0666
ABDULLAH IQBAL <65629693+abdullahIqbalbaloch@users.noreply.github.com>
2026-09-11 06:16:02 +0400

    Add /privacy-policy route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import Landing from "@/pages/Landing";
 import ThankYou from "@/pages/ThankYou";
+import PrivacyPolicy from "@/pages/PrivacyPolicy";
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/thank-you" element={<ThankYou />} />
+      <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="*" element={<Landing />} />
     </Routes>
   );
